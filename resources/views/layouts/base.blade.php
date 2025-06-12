efa35cb9564acf5ecc25d2bc2209e3695587e184
disyudhis <yudhistiraaaa20@gmail.com>
2025-06-12 10:16:58 +0700

    change asset for env

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -15,7 +15,7 @@
 
     <!-- Scripts -->
     @if (app()->environment('production'))
-        <link rel="stylesheet" href="{{ asset('build/assets/app-BXA68R2V.css') }}">
+        <link rel="stylesheet" href="{{ asset('build/assets/app-D9sYT4TE.css') }}">
         <script type="module" src="{{ asset('build/assets/app-T1DpEqax.js') }}"></script>
     @else
         @vite(['resources/css/app.css', 'resources/js/app.js'])
